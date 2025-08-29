a0476aa569377841148d72756c464dd87e3ead14
LianQi <18201584863@163.com>
2025-08-30 04:48:17 +0800

    add requestToken def

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     resource_url: str = "https://api.aconex.com"
     client_id: str = "CLIENT_ID"
     client_secret: str = "CLIENT_SECRET"
+    access_token_expires: int = 3600
     access_token: str = "ACCESS_TOKEN"
     aconex_user_id: str = "ACONEX_USER_ID"
     aconex_instance_url: str = "https://asia1.aconex.com"
--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+import base64
+from datetime import datetime, timedelta
+
+import requests
+
 from config import config
+
+
+def requestToken() -> dict:
+    """
+    Request an OAuth2 token using client credentials grant type
+
+    https://help.aconex.com/zh/apis/implement-smart-construction-platform-oauth/#Implement-OAuth-in-a-User-Bound-Integration
+    """
+
+    def basic_auth_encode(client_id: str, client_secret: str) -> str:
+        """
+        Encode client_id and client_secret in base64 for Basic Auth
+
+        https://en.wikipedia.org/wiki/Basic_access_authentication
+        """
+        auth_str = f"{client_id}:{client_secret}"
+        b64_encoded = base64.b64encode(auth_str.encode()).decode()
+        return b64_encoded
+
+    response = requests.post(url=f"{config.lobby_url}/auth/token",
+                             headers={"Content-Type": "application/x-www-form-urlencoded",
+                                      "Authorization": f"Basic {basic_auth_encode(config.client_id, config.client_secret)}"},
+                             data={"grant_type": "client_credentials", "user_id": config.aconex_user_id,
+                                   "user_site": config.aconex_instance_url}, )
+
+    response.raise_for_status()
+
+    config.access_token = response.json().get("access_token")
+    config.access_token_expires = datetime.now() + timedelta(seconds=response.json().get("expires_in") - 10)
+    return response.json()
+
+
+if __name__ == '__main__':
+    print(config)
