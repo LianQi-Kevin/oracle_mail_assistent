0c0b68b3e2590c93f8ece6c147916bb3e544e0b7
LianQi <18201584863@163.com>
2025-08-30 03:51:00 +0800

    finish config

---
+++ config.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    lobby_url: str = "https://constructionandengineering.oraclecloud.com"
+    resource_url: str = "https://api.aconex.com"
+    client_id: str = "CLIENT_ID"
+    client_secret: str = "CLIENT_SECRET"
+    access_token: str = "ACCESS_TOKEN"
+    aconex_user_id: str = "ACONEX_USER_ID"
+    aconex_instance_url: str = "https://asia1.aconex.com"
+    project_id: str = "PROJECT_ID"
+
+
+config = Config()
